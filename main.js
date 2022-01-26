2489ddd34a283004cd3e9ade396995fc6f1403e0
Daniel Juarez <juarezdaniel@gmail.com>
2022-01-26 17:14:41 -0500

    search bar

--- main.js
+++ main.js
@@ -1,24 +1,40 @@
 $(document).ready(function () {
     const BASE_URL = 'https://openapi.etsy.com/v2/public/listings/active.js';
-    let searchTerm = 'car';
     let limit = '48';
-  
 
-    $.ajax({
-        url: `${BASE_URL}?api_key=${API_KEY}&limit=${limit}&includes=Images:1&keywords=${searchTerm}`,
-        dataType: 'jsonp',
-        method: 'GET',
+    const searchButtons = document.querySelectorAll('.button');
+    const submitButton = document.querySelector('.submit');
+    const searchBar = document.querySelector('.search');
 
-        
-        success: function (data) {
-            const source = document.getElementById('etsy-image-template').innerHTML;
-            const template = Handlebars.compile(source);
-            const html = template(data);
-            document.querySelector('.row').innerHTML = html;
-        },
-
-        error: function (xhr) {
-            console.log('Uh oh! Something went wrong.', xhr.status);
-        }
+    searchButtons.forEach(button => {
+        button.addEventListener('click', (event) => fetchData(event.target.value));
     });
+
+    submitButton.addEventListener('click', () => {
+        fetchData(searchBar.value);
+        searchBar.value= '';
+    })
+
+
+    function fetchData(searchTerm) {
+        $.ajax({
+            url: `${BASE_URL}?api_key=${API_KEY}&limit=${limit}&includes=Images:1&keywords=${searchTerm}`,
+            dataType: 'jsonp',
+            method: 'GET',
+
+            success: function (data) {
+                const source = document.getElementById('etsy-image-template').innerHTML;
+                const template = Handlebars.compile(source);
+                const html = template(data);
+                document.querySelector('.row').innerHTML = html;
+            },
+
+            error: function (xhr) {
+                console.log('Uh oh! Something went wrong.', xhr.status);
+            }
+        });
+    }
+
+    fetchData('car');
+
 });
